3e3fc323d57440c30418f0411d4571f967cd7d66
JJ1LFC <alt@sfc.wide.ad.jp>
2020-01-29 02:20:09 +0900

    [feat] specify IP version

--- calc.sh
+++ calc.sh
@@ -3,13 +3,13 @@ HOST=`hostname -f`
 IP=`hostname -I`
 echo "******kdig result from $HOST ( $IP)******"
 
-for i in udp udpcache tcp tcpcache
+for i in udp4 udpcache4 tcp4 tcpcache4 udp6 udpcache6 tcp6 tcpcache6
 do
     sed -i -e 's/;;From.*in //' ./$i
     sed -i -e 's/ ms//' ./$i
 done
 
-for i in udp udpcache tcp tcpcache
+for i in udp4 udpcache4 tcp4 tcpcache4 udp6 udpcache6 tcp6 tcpcache6
 do
     echo -e "\n$i min, max, avg"
     awk 'BEGIN{m=100000}{if(m>$1) m=$1} END{print m}' < ./$i
--- dig.sh
+++ dig.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/bash
-for i in udp udpcache tcp tcpcache
+for i in udp4 udpcache4 tcp4 tcpcache4 udp6 udpcache6 tcp6 tcpcache6
 do
     touch ./$i
 done
 
-echo 'Starting with UDP...'
+echo 'Starting with UDP via IPv4...'
 
 for i in `seq 500`
 do
     ssh -J cpu1 endo 'sudo unbound-control reload'
-    kdig google.com. @endo.sfc.wide.ad.jp.:853 +notls | grep '(;; From|;; WARNING)' >> ./udp
-    kdig google.com. @endo.sfc.wide.ad.jp.:853 +notls | grep '(;; From|;; WARNING)' >> ./udpcache
-    echo "UDP $i "
+    kdig -4 google.com. @endo.sfc.wide.ad.jp.:853 +notls | grep -E '(;; From|;; WARNING)' >> ./udp4
+    kdig -4 google.com. @endo.sfc.wide.ad.jp.:853 +notls | grep -E '(;; From|;; WARNING)' >> ./udpcache4
+    echo "IPv4 UDP $i "
 done
 
-echo 'Starting with TCP...'
+echo 'Starting with TCP via IPv4...'
 
 for i in `seq 500`
 do
     ssh -J cpu1 endo 'sudo unbound-control reload'
-    kdig google.com. @endo.sfc.wide.ad.jp.:853 +tcp +tls | grep '(;; From|;; WARNING)' >> ./tcp
-    kdig google.com. @endo.sfc.wide.ad.jp.:853 +tcp +tls | grep '(;; From|;; WARNING)' >> ./tcpcache
-    echo "TCP $i "
+    kdig -4 google.com. @endo.sfc.wide.ad.jp.:853 +tcp +tls | grep -E '(;; From|;; WARNING)' >> ./tcp4
+    kdig -4 google.com. @endo.sfc.wide.ad.jp.:853 +tcp +tls | grep -E '(;; From|;; WARNING)' >> ./tcpcache4
+    echo "IPv4 TCP $i "
 done
+
+echo 'Starting with UDP via IPv6...'
+echo 'If you do not have IPv6 connectivity, just interrupt this.'
+
+for i in `seq 500`
+do
+    ssh -J cpu1 endo 'sudo unbound-control reload'
+    kdig -6 google.com. @endo.sfc.wide.ad.jp.:853 +notls | grep -E '(;; From|;; WARNING)' >> ./udp6
+    kdig -6 google.com. @endo.sfc.wide.ad.jp.:853 +notls | grep -E '(;; From|;; WARNING)' >> ./udpcache6
+    echo "IPv6 UDP $i "
+done
+
+echo 'Starting with TCP via IPv6...'
+
+for i in `seq 500`
+do
+    ssh -J cpu1 endo 'sudo unbound-control reload'
+    kdig -6 google.com. @endo.sfc.wide.ad.jp.:853 +tcp +tls | grep -E '(;; From|;; WARNING)' >> ./tcp6
+    kdig -6 google.com. @endo.sfc.wide.ad.jp.:853 +tcp +tls | grep -E '(;; From|;; WARNING)' >> ./tcpcache6
+    echo "IPv6 TCP $i "
+done
+
 
 bash ./calc.sh
 
